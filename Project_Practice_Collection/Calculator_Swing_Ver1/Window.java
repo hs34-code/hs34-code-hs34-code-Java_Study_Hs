5bac71bd30961915dd3c216b0e373da0303802c6
hs34-code <sublhs030304@gmail.com>
2025-10-26 23:17:40 +0900

    25/10/26

--- Project_Practice_Collection/Calculator_Swing_Ver1/Window.java
+++ Project_Practice_Collection/Calculator_Swing_Ver1/Window.java
@@ -12,58 +12,82 @@ import java.awt.*;
 public class Window {
     JFrame frame = new JFrame("계산기 연습"); // 창 생성
     
-    JPanel top = new JPanel();
-    JPanel bottom = new JPanel();
+    JPanel top = new JPanel();  // 위 방 생성 
+    JPanel bottom = new JPanel(); // 아래 방 생성
 
     // Top Panel
-    JTextArea textHistoryDisplay = new JTextArea();
-    JTextArea textInputNumbers = new JTextArea();
+    JTextArea textHistoryDisplay, textInputNumbers; // 위 방에 이런 가구를 들여놓을 예정
 
     // Bottom Panel
-    JButton buttonNum0 = new JButton();
-    JButton buttonNum1 = new JButton();
-    JButton buttonNum2 = new JButton();
-    JButton buttonNum3 = new JButton();
-    JButton buttonNum4 = new JButton();
-    JButton buttonNum5 = new JButton();
-    JButton buttonNum6 = new JButton();
-    JButton buttonNum7 = new JButton();
-    JButton buttonNum8 = new JButton();
-    JButton buttonNum9 = new JButton();
-    JButton buttonBlank1 = new JButton();
-    JButton buttonBlank2 = new JButton();
-    JButton buttonRemainder = new JButton();
-    JButton buttonDelete = new JButton();
-
-    JButton buttonParenthesesStrat = new JButton();
-    JButton buttonParenthesesEnd = new JButton();
-    JButton buttonClear = new JButton();
-    JButton buttonDivide = new JButton();
-
-    JButton buttonMultiplication = new JButton();
-    JButton buttonSubtraction = new JButton();
-    JButton buttonAddition = new JButton();
-    JButton buttonSignChange = new JButton();
-    JButton buttonDecimal = new JButton();
-    JButton buttonResult = new JButton();
+    JButton[] buttonNum = new JButton[10]; // 아래 방에 이런 가구를 들여놓을 예정
+    
+    JButton buttonBlank1, buttonBlank2, buttonRemainder, buttonDelete;
+    JButton buttonParenthesesStrat, buttonParenthesesEnd, buttonClear, buttonDivide;
+    JButton buttonMultiplication, buttonSubtraction,buttonAddition, buttonSignChange, buttonDecimal, buttonResult;
  
     public Window(){
         frame.setBounds(800, 200, 350 , 570); // 크기 설정
         
-        settingTopomponents();
-        settingBottomComponents();
-        settingPanel();
+        frame.setLayout(new GridLayout(2, 1));  // 화면 top, bottom으로 가르기
+        
+        settingComponentNumButton();            // bottom 숫자 버튼 생성 및 수 입력     / Component 생성과 동시에 속성 설정
+        settingComponentpOperatorButton();      // bottom 기호 버튼 생성 및 기호 입력   / Component 생성과 동시에 속성 설정
+        settingTopomponents();      //top JTextAera 생성 및 폰트, 크기 설정             / Component 생성과 동시에 속성 설정
+        
+        settingPanel(); // 각 top, bottom 가구들을 각 방에 맞게 추가
 
+        frame.add(top);         // top frame 추가           // 집에 위 방 추가
+        frame.add(bottom);      // bottom frame 추가        // 집에 아래 방 추가
 
-        frame.setLayout(new GridLayout(2, 1));
-        frame.add(top);
-        frame.add(bottom);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //닫기 기능 추가
+        frame.setVisible(true); // 창 표시              // 집 공개
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true); // 창 표시
+        /*
+        Component 생성                      │   JFrame  frame 은 집
+        속성 설정 (이벤트및 문구)            │   JPanel  panel 은 방
+        패널 추가                           │   JButton Button 및 TextArea 들은 가구
+        프레임 설정                         
+        화면 표시 순서      setVisible
+        */
     }
 
-    private void settingPanel(){
+    private void settingComponentNumButton(){ // 아래 방 가구 생성
+        for(int i=0; i<10; i++){
+            buttonNum[i] = new JButton(""+i);
+        }
+    }
+
+    private void settingComponentpOperatorButton(){  // 아래 방 가구 생성
+        buttonBlank1 = new JButton("");
+        buttonBlank2 = new JButton("");
+        buttonRemainder = new JButton("나머지");
+        buttonDelete = new JButton("< DEL");
+
+        buttonParenthesesStrat = new JButton("(");
+        buttonParenthesesEnd = new JButton(")");
+        buttonClear = new JButton("C");
+        buttonDivide = new JButton("÷");
+
+        buttonMultiplication = new JButton("x");
+        buttonSubtraction = new JButton("-");
+        buttonAddition = new JButton("+");
+        buttonSignChange = new JButton("+/- ");
+        buttonDecimal = new JButton(".");
+        buttonResult = new JButton("=");
+    }
+
+    private void settingTopomponents(){ // 위 방 가구 생성 
+        textHistoryDisplay = new JTextArea();
+        textHistoryDisplay.setFont(new Font("돋움", Font.PLAIN, 20));
+        textHistoryDisplay.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        textInputNumbers = new JTextArea("0");
+        textInputNumbers.setFont(new Font("돋움", Font.BOLD, 35)); // 글자 크기 및 폰트 지정
+        textInputNumbers.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); // 오른쪽 - 왼쪽 입력
+        textInputNumbers.setEditable(false); // 입력불가
+    }
+
+    private void settingPanel(){ // 각각 방 가구 배치
         //top
         top.setLayout(new GridLayout(2,1));
         top.add(textHistoryDisplay);
@@ -81,69 +105,36 @@ public class Window {
         bottom.add(buttonClear);
         bottom.add(buttonDivide);
 
-        bottom.add(buttonNum7);
-        bottom.add(buttonNum8);
-        bottom.add(buttonNum9);
+        bottom.add(buttonNum[7]);
+        bottom.add(buttonNum[8]);
+        bottom.add(buttonNum[9]);
         bottom.add(buttonMultiplication);
 
-        bottom.add(buttonNum4);
-        bottom.add(buttonNum5);
-        bottom.add(buttonNum6);
+        bottom.add(buttonNum[4]);
+        bottom.add(buttonNum[5]);
+        bottom.add(buttonNum[6]);
         bottom.add(buttonSubtraction);
 
-        bottom.add(buttonNum1);
-        bottom.add(buttonNum2);
-        bottom.add(buttonNum3);
+        bottom.add(buttonNum[1]);
+        bottom.add(buttonNum[2]);
+        bottom.add(buttonNum[3]);
         bottom.add(buttonAddition);
 
         bottom.add(buttonSignChange);
-        bottom.add(buttonNum0);
+        bottom.add(buttonNum[0]);
         bottom.add(buttonDecimal);
         bottom.add(buttonResult);
     }
 
-
-    private void settingTopomponents(){
-        textHistoryDisplay.setFont(new Font("돋움", Font.PLAIN, 20));
-        textHistoryDisplay.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-
-        textInputNumbers.setText("0"); // 초기 입력값
-        textInputNumbers.setFont(new Font("돋움", Font.BOLD, 35)); // 글자 크기 및 폰트 지정
-        textInputNumbers.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); // 오른쪽 - 왼쪽 입력
-        textInputNumbers.setEditable(false); // 입력불가
+    private void settingNumButtonEvent(){
+        for(int i=0; i<10; i++){
+            int num = i;
+            buttonNum[i].addActionListener( e -> {
+                String currentText = textInputNumbers.getText();
+                if (currentText.equals("0")){
+                    
+                }
+            });
+        }
     }
-
-    private void settingBottomComponents() {
-        buttonBlank1.setText("");
-        buttonBlank2.setText("");
-        buttonRemainder.setText("나머지");
-        buttonDelete.setText("< DEL");
-
-        buttonParenthesesStrat.setText("(");
-        buttonParenthesesEnd.setText(")");
-        buttonClear.setText("C");
-        buttonDivide.setText("÷");
-
-        buttonMultiplication.setText("x");
-        buttonSubtraction.setText("-");
-        buttonAddition.setText("+");
-        buttonSignChange.setText("+/- ");
-        buttonDecimal.setText(".");
-        buttonResult.setText("=");
-
-        buttonNum0.setText("0");
-        buttonNum1.setText("1");
-        buttonNum2.setText("2");
-        buttonNum3.setText("3");
-        buttonNum4.setText("4");
-        buttonNum5.setText("5");
-        buttonNum6.setText("6");
-        buttonNum7.setText("7");
-        buttonNum8.setText("8");
-        buttonNum9.setText("9");
-    }
-
-
-
-
 }
